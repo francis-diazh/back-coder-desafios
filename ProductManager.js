5d2166248f7197d873f1c6a7730e8f99ec271901
Francis Diaz <diazh.francis@gmail.com>
2023-10-24 19:34:51 -0300

    2do desafio arreglos

--- ProductManager.js
+++ ProductManager.js
@@ -1,32 +1,66 @@
+const fs = require ('fs');
+
+//Definimos la clase ProducManager
 class ProductManager {
   constructor() {
     this.products = [];
+    this.idAuto = 1;
+    this.path = './products.json'; //Se refiere al this.path
   }
+  
+  //addProduct para agregar nuevos productos
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    // Valida si todos los campos requeridos están presentes.
+    if (
+      !title ||
+      !description ||
+      !price ||
+      !thumbnail ||
+      !code ||
+      stock === undefined
+    ) {
+      console.error("Todos los campos son obligatorios.");
+      return Promise.reject("Todos los campos son obligatorios.");
+    }
+
+    // Comprueba si un producto con el mismo código ya existe.
+    if (this.products.some((product) => product.code === code)) {
+      console.error(`El producto con código ${code} ya existe.`);
+      return Promise.reject(`El producto con código ${code} ya existe.`);
+    }
+
+    // Crea un nuevo producto con un ID único y otros detalles.
+    const newProduct = {
+      id: this.idAuto++,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    // Agrega el producto al arreglo de productos.
+    this.products.push(newProduct);
+
+    // Intenta guardar los productos en un archivo.
+    
+    
+  }
+
+  // Método para obtener todos los productos.
   getProducts() {
-    console.log(this.products);
     return this.products;
   }
 
-  getProductById(product_id) {
-    let one = this.products.find((e) => e.id === product_id);
-    if (one) {
-      console.log(one);
-      return one;
-    }
-    console.log("not found");
-    return null;
-  }
-
-  addProduct({ title, description, price, thumbnail, stock }) {
-    let id = 0;
-    if (this.products.length === 0) {
-      id = 1;
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (product) {
+      return product;
     } else {
-      let lastProducts = this.products[this.products.length - 1];
-      id = lastProducts.id + 1;
+      console.log("Product not found");
+      return null;
     }
-    let product = { title, description, price, thumbnail, stock, id };
-    this.products.push(product);
   }
 
   updateProduct(id, data) {
@@ -67,3 +101,44 @@ class ProductManager {
     }
   }
 }
+
+const productManager = new ProductManager();
+
+// Función autoinvocada asincrónica para probar las operaciones del administrador de productos.
+(async () => {
+  try {
+    await productManager.addProduct(
+      'Remera','Talle L',5000,"img1","R1",10
+    );
+    await productManager.addProduct(
+      'Buzo Hoodie','Talle M',8000,"img2","B2",20
+    );
+    await productManager.addProduct(
+      'Pantalon cargo','Talle XL',6400,"img3","PC1",5
+    );
+    await productManager.addProduct(
+      'Piluso','Unisex',2000,"img4","PIL1",100
+    );
+   
+
+    // Muestra todos los productos.
+    const allProducts = productManager.getProducts();
+    console.log("Todos los productos:", allProducts);
+
+    // Actualiza y muestra los products.
+    const productIdToUpdate = 1;
+    await productManager.updateProduct(productIdToUpdate, {
+      price: 10000,
+      stock: 30,
+    });
+
+    // Elimina un producto y muestra los productos actualizados.
+    const productIdToDelete = 2;
+    await productManager.deleteProduct(productIdToDelete);
+
+    console.log("Productos actualizados:", productManager.getProducts());
+  } catch (err) {
+    console.error("Error en la operación:", err);
+  }
+})
+();
